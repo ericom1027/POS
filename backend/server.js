5e6d4c5a5b5fd2acb95f19e5d0a03958ac3cc79a
ericom1027 <ericson211923@gmail.com>
2024-04-22 11:05:02 +0800

    Backend update

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,15 @@ app.use("/items", itemRoutes);
 app.use("/users", userRoutes);
 app.use("/bills", billsRoutes);
 app.use("/shifts", shiftRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = 5000;
 
 app.listen(process.env.PORT || port, () => {
