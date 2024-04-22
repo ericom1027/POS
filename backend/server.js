6d8cc1a0e40d29112e30f0dee76171d15397bd96
ericom1027 <ericson211923@gmail.com>
2024-04-22 14:13:18 +0800

    Backend update

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,13 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://pos-1-8vgs.onrender.com", // Replace with your React app's domain
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 // Serve static files from the "client/build" directory
 app.use(express.static(path.join(__dirname, "client", "build")));
